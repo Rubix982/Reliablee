464f81c08a7db645e8b73b9050628f0b62780bc8
Saif Ul Islam <saifulislam84210@gmail.com>
2021-05-12 01:45:57 +0500

    env changes, .gitignore changes, and loggin bug hunt with app.py

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 # Package imports
+from dotenv import load_dotenv
 import asyncio
+import os
 
 # Local imports
 from controllers.ServerDriver import Servers
 from controllers.ClientDriver import Clients
 
+load_dotenv()
+
 def main():
+
+    if not os.path.exists('logs'):
+
+        os.makedirs('logs')
+
+        with open(str(os.environ['RECEIVER_LOG_FILENAME']), encoding='UTF-8', mode='w') as file:
+            pass
+
+        with open(str(os.environ['SENDER_LOG_FILENAME']), encoding='UTF-8', mode='w') as file:
+            pass
+
     asyncio.run(Servers())
     asyncio.run(Clients())
 
